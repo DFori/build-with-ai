8bd61feb2272d654556881abeefcd130195cc33d
Daniel Fori <122609602+DFori@users.noreply.github.com>
2024-11-24 09:07:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-os.environ['GOOGLE_API_KEY'] = "AIzaSyD5EsYS77CIaJ__P_xZZVAoI3lbYtFiyv8"
+os.environ['GOOGLE_API_KEY'] = ""
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 # for models in genai.list_models():
