ec1e2e4aa48dfb8700c24cb224cb22aeb45b0650
Daniel Fori <122609602+DFori@users.noreply.github.com>
2024-11-24 09:06:06 +0100

    Update vision.py

--- vision.py
+++ vision.py
@@ -1,7 +1,7 @@
 import os
 import google.generativeai as genai
 
-os.environ['GOOGLE_API_KEY'] = "AIzaSyD5EsYS77CIaJ__P_xZZVAoI3lbYtFiyv8"
+os.environ['GOOGLE_API_KEY'] = ""
 genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
 vision_model = genai.GenerativeModel('gemini-1.5-flash-latest')
